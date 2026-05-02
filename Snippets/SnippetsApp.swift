1a6e46741151d74b0eda42dd624863c94634d8a2
glacierboi1 <pragathichengappa@student.uts.edu.au>
2026-05-02 15:13:13 +1000

    Initial Commit

---
+++ Snippets/SnippetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnippetsApp.swift
+//  Snippets
+//
+//  Created by Pragathi Chengappa on 02/05/26.
+//
+
+import SwiftUI
+
+@main
+struct SnippetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
